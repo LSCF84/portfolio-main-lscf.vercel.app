d1dbe7615e921dde66282fd9af773374ac119068
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-25 18:08:04 +0100

    Update route.ts

--- src/app/api/work-data/route.ts
+++ src/app/api/work-data/route.ts
@@ -4,37 +4,37 @@ const workData = [
     {
         image: "/images/work/CSM.png",
         title: "Check Software Minimum-CSM",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/Check-Software-Minimum-CSM"
     },
     {
         image: "/images/work/VR-GUI.png",
         title: "Renamer GUI VR-GUI",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/Version-Renamer-GUI_VR-GUI"
     },
     {
         image: "/images/work/WEDA.png",
         title: "Windows Error Diagnostic with AI-WEDA",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/Windows-Error-Diagnostic-with-AI_WEDA"
     },
     {
         image: "/images/work/PYE.png",
         title: "py-to-exe",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/py-to-exe"
     },
     {
         image: "/images/work/RS.png",
         title: "PDF Renamer Security PDF-RS",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/PDF-Renamer-Security-PDF-RS"
     },
     {
         image: "/images/work/RSS.png",
         title: "PDF Renamer Security Suite PDF-RSS",
-        Tipo: "Proyecto Personal",
+        client: "Proyecto Personal",
         slug: "https://github.com/LSCF84/PDF-Renamer-Security-Suite-PDF-RSS"
     }
 ];
